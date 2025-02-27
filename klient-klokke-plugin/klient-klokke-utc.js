d208e822afb58f779a8a9eac5d87b8aa5b7e2b35
Jan Ove Overland <102610897+Overland-DX@users.noreply.github.com>
2025-02-27 10:39:53 +0100

    Add files via upload

---
+++ klient-klokke-plugin/klient-klokke-utc.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Klient-Klokke-UTC Plugin',
+    version: '1.0.3',
+    author: 'ODX',
+    frontEndPath: 'klient-klokke-utc/klient-klokke-UTC-plugin.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
