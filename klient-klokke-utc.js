2e2a0de55f824d1119754fd9382132f22e43e7ad
Jan Ove Overland <102610897+Overland-DX@users.noreply.github.com>
2025-02-27 12:40:21 +0100

    Update klient-klokke-utc.js

--- klient-klokke-utc.js
+++ klient-klokke-utc.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Klient-Klokke-UTC Plugin',
-    version: '1.0.3',
+    version: '1.0.4',
     author: 'ODX',
     frontEndPath: 'klient-klokke-utc/klient-klokke-UTC-plugin.js'
 }
