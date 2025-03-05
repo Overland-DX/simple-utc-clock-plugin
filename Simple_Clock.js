c3417eb1274f0387835f24eafd00fb651a3ad3e2
Jan Ove Overland <102610897+Overland-DX@users.noreply.github.com>
2025-03-05 15:08:06 +0100

    Update Simple_Clock.js

--- Simple_Clock.js
+++ Simple_Clock.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Simple Clock Plugin',
-    version: '1.04.0',
+    version: '1.04.1',
     author: 'ODX',
     frontEndPath: 'Simple_Clock/Simple_clock_plugin.js'
 }
